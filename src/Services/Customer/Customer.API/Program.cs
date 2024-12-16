03b629b422aee76ac85711f3bf549a3593e04ffe
Hoàng Anh Phạm <hoanganhpham.rasta@gmail.com>
2024-12-17 00:03:34 +0700

    Session03

--- src/Services/Customer/Customer.API/Program.cs
+++ src/Services/Customer/Customer.API/Program.cs
@@ -1,4 +1,19 @@
+using Customer.API;
+using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interfaces;
+using Customer.API.Services;
+using Customer.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information($"Start {builder.Environment.ApplicationName} up");
 
 // Add services to the container.
 
@@ -6,8 +21,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile(new MappingProfile()));
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<CustomerContext>(
+           options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+            .AddScoped<ICustomerService, CustomerService>();
 var app = builder.Build();
+
+app.MapGet("/", () => "Hello");
+app.MapGet("/api/customers", async (ICustomerService customerService) => await customerService.GetCustomersAsync());
+app.MapGet("/api/customer/{username}", async (string username, ICustomerService customerService) => await customerService.GetCustomerByUsernameAsync(username));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +47,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.SeedCustomerData();
 
 app.Run();
